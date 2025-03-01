ad26184cac33fca6bfbd33e81920bd346524edbd
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-03-01 16:21:39 +0530

    subscription controller added

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { createSubscription, getUserSubscription } from "../controllers/susbcription.controller.js";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req, res) => { res.send({ title: 'get all subscriptions' })});
 subscriptionRouter.get('/:id', (req, res) => { res.send({ title: 'get subscriptions details' })});
-subscriptionRouter.post('/', (req, res) => { res.send({ title: 'create subscriptions' })});
+subscriptionRouter.post('/', authorize, createSubscription);
 subscriptionRouter.put('/:id', (req, res) => { res.send({ title: 'update subscriptions' })});
 subscriptionRouter.delete('/:id', (req, res) => { res.send({ title: 'delete subscriptions' })});
-subscriptionRouter.get('/user/:id', (req, res) => { res.send({ title: 'get all user subscriptions' })});
+subscriptionRouter.get('/user/:id', authorize, getUserSubscription);
 subscriptionRouter.put('/:id/cancel', (req, res) => { res.send({ title: 'cancel subscriptions' })});
 subscriptionRouter.get('/upcoming-renewals', (req, res) => { res.send({ title: 'get upcoming renewals' })});
 
